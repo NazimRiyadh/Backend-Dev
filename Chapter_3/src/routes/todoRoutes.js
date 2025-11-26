fe888c9c91d77136751f3a1db6917e83f08001bd
NazimRiyadh <neariyadh786@gmail.com>
2025-11-27 01:18:23 +0600

    full stack todo done

--- Chapter_3/src/routes/todoRoutes.js
+++ Chapter_3/src/routes/todoRoutes.js
@@ -18,17 +18,31 @@ router.get('/', (req, res) => {
 
 //create new todo
 router.post('/', (req, res) => {
-
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos(task, user_id) VALUES(?, ?)`)
+    const result = insertTodo.run(task, req.userId)
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
 })
 
 //update todo
 router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
 
+    const updateTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+    updateTodo.run(completed, id)
+
+    res.json({ id, completed, page })
 })
 
 //delete todo
 router.delete('/:id', (req, res) => {
-
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? and user_id = ?`)
+    deleteTodo.run(id, userId)
+    res.json({ message: "Todo deleted successfully" })
 })
 
 export default router
