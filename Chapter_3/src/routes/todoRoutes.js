9e42d29619188dfe87a353d81c2acd3d4de4ade6
NazimRiyadh <neariyadh786@gmail.com>
2025-11-27 00:33:06 +0600

    Works till frtching todos

--- Chapter_3/src/routes/todoRoutes.js
+++ Chapter_3/src/routes/todoRoutes.js
@@ -5,7 +5,15 @@ const router = express.Router()
 
 //get todos for logged in user
 router.get('/', (req, res) => {
-
+    try {
+        const userId = req.userId // from auth middleware
+        const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
+        const todos = getTodos.all(userId)
+        res.json(todos)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Failed to fetch todos" })
+    }
 })
 
 //create new todo
